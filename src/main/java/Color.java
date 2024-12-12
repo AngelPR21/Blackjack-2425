7c3759aa1026a23d5fe84a0a37751677b0a533d3
AngelPR21 <angpasrod2@alu.edu.gva.es>
2024-12-12 19:42:49 +0100

    version 0 12/12/24

--- src/main/java/Color.java
+++ src/main/java/Color.java
@@ -2,7 +2,7 @@ import com.diogonunes.jcolor.Attribute;
 
 public enum Color {
     ROJO(Attribute.RED_TEXT()),
-    NEGRO(Attribute.BLACK_TEXT());
+    NEGRO(Attribute.BLACK_TEXT()); //Atributo es importado, funciona como String
 
     private Attribute color;
 
